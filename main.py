57be53b264f59566c3c29c59b5063917b20ccb30
Andr-e-acute <andre.potthast.ap@googlemail.com>
2025-05-11 12:38:00 +0200

    asteroid not working

--- main.py
+++ main.py
@@ -2,25 +2,44 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 # Constants for the game
 from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     clock= pygame.time.Clock()
+
+    updatable= pygame.sprite.Group()
+    drawable= pygame.sprite.Group()
+    asteroids=pygame.sprite.Group()
+    
+    Asteroid.containers=(updatable,drawable,asteroids)
+    AsteroidField.containers =(updatable)
+    asteroid_field =AsteroidField()
+    
+    Player.containers=(updatable,drawable)
+    # Create a player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
     dt=0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
         pygame.display.flip()
-        dt=clock.tick(60)/100 
-        print(dt)    
-    print( "Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        dt=clock.tick(60)/1000
+        updatable.update(dt)
+        for player in drawable:
+            player.draw(screen)
+
 
 
 if __name__ == "__main__":
