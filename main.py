68ebb4572f8ed6c629cb919ab3d4cf3dbe3cd1cf
Andr-e-acute <andre.potthast.ap@googlemail.com>
2025-05-11 10:43:31 +0200

    added class for the circleshape

--- main.py
+++ main.py
@@ -2,16 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+# Constants for the game
 from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock= pygame.time.Clock()
+    dt=0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()         
+        pygame.display.flip()
+        dt=clock.tick(60)/100 
+        print(dt)    
     print( "Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
